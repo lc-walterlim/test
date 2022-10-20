84eaba509e0bbcbaaf2b4402d335e0db3fbf3e6c
Walter Lim <waltissomewhere@gmail.com>
2022-10-21 12:46:47 +1300

    add js

---
+++ test.js
@@ -0,0 +1,13 @@
+const style = document.createElement("style");
+
+style.textContent = `
+.mod-navsidebar {
+    display: none;
+}
+
+.document-sidebar {
+    display: none;
+}
+`;
+
+document.head.appendChild(style);
